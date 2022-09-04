10149910fb73e2bfefd4703ab2f70cbea0a8fc1d
seunaliu3@gmail.com <seunaliu3@gmail.com>
2022-09-04 10:28:56 +0100

    commit tree 2e4dd5bd44e42a53b518062d1b719f52bc95552f

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+const employee={
+    name:"Seun",
+    streetAddress:"Solution Bus-stop",
+
+}
+
+function updateEmployeeWithKeyAndValue(employee,name, seun)
+ {const newObj= {...employee}
+
+  newObj.Seun= "Aliu"
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee,name,seun){
+    const firstBank=destructivelyUpdateEmployeeWithKeyAndValue(employee,"streetAddress", "Muslim Ogbere Idi-Osan")
+}
